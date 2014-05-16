4b6b258f0ff6c4496cf3e50fb43b2a929b7a6d54
Administrator <Administrator@AthlonII630>
2014-05-17 06:46:46 +0800

    if (!empty($src))

--- html5_mp3player.tpl.php
+++ html5_mp3player.tpl.php
@@ -1,5 +1,7 @@
+<?php if (!empty($src)): ?>
 <audio controls="controls" preload="meta">
   <source src="<?php print $src; ?>" type="audio/mpeg" />
   <embed width="290" height="25" type="application/x-shockwave-flash" src="<?php print $swf; ?>"
       flashvars="titles=<?php print $titles; ?>&amp;artists=<?php print $artists; ?>&amp;soundFile=<?php print $src; ?>"></embed>
 </audio>
+<?php endif; ?>
