d983f8df6ae2dd8e3c57d16dd1914b30037020d9
Administrator <Administrator@AthlonII630>
2014-05-17 06:06:49 +0800

    some updates

---
+++ html5_mp3player.tpl.php
@@ -0,0 +1,5 @@
+<audio controls="controls" preload="meta">
+  <source src="<?php print $src; ?>" type="audio/mpeg" />
+  <embed width="290" height="25" type="application/x-shockwave-flash" src="<?php print $swf; ?>"
+      flashvars="titles=<?php print $titles; ?>&amp;artists=<?php print $artists; ?>&amp;soundFile=<?php print $src; ?>"></embed>
+</audio>
